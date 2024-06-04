448753865df62b52711574c04fd0aeda61fe1561
pandeyroopam <roopampandey21@gmail.com>
2024-06-04 22:10:00 +0530

    adding new file

--- longestPalindrome.java
+++ longestPalindrome.java
@@ -1,3 +1,4 @@
+package Leetcode.june;
 // Given a string s which consists of lowercase or uppercase letters, return the length of the longest 
 // palindrome
 //  that can be built with those letters.
